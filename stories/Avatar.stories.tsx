3e79011aea77eaa4fa1240060412470903ab86d9
launchpad5682 <launchpad5682@gmail.com>
2022-01-14 22:46:58 +0530

    feat: Avatar story added

---
+++ stories/Avatar.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta } from '@storybook/react';
+import { Avatar } from '../src/Avatar/Avatar';
+
+const meta: Meta = {
+  title: 'Avatar',
+  component: Avatar,
+};
+
+export default meta;
+
+export const Default = (args) => <Avatar {...args} />;
+
+export const Round = Default.bind({});
+
+export const Square = Default.bind({});
+
+Round.args = {
+  type: 'round',
+  size: 'md-avatar',
+};
+
+Square.args = {
+  type: 'square',
+  size: 'sm-avatar',
+};
