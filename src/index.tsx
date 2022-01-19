4d560fa059175f3f4376556cbbe5a5b3005d091a
launchpad5682 <launchpad5682@gmail.com>
2022-01-19 19:03:46 +0530

    feat: Badge, Image, Card added to entrypoint for npm package

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,5 @@
 export { Alert } from './Alert/Alert';
 export { Avatar } from './Avatar/Avatar';
+export { Badge } from './Badge/Badge';
+export { Image } from './Image/Image';
+export { Card } from './Card/Card';
