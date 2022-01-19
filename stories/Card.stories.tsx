1d426f55c7994f02c469e7c2c23385576fc0a1c5
launchpad5682 <launchpad5682@gmail.com>
2022-01-19 18:50:27 +0530

    story: Card story added and Card.css added for styling the passed components in the card

--- stories/Card.stories.tsx
+++ stories/Card.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta } from '@storybook/react';
 import { Card } from '../src/Card/Card';
+import { WiAlien, WiBarometer, WiCelsius } from 'react-icons/wi';
+import './styles/Card.css';
 
 const meta: Meta = {
   title: 'Card',
@@ -10,13 +12,152 @@ export default meta;
 
 const Default = (args) => <Card {...args} />;
 
-export const CardWithTextOverlay = Default.bind({});
+export const CardWithTextOverlayVertical = Default.bind({});
+export const CardWithTextOverlayHorizontal = Default.bind({});
+export const CardWithTextOverlayAndDismissVertical = Default.bind({});
+export const CardWithTextOverlayAndDismissHorizontal = Default.bind({});
+export const CardWithOnlyText = Default.bind({});
+export const CardWithOnlyTextAndDismiss = Default.bind({});
 
-CardWithTextOverlay.args = {
-  children: 'This is so cool',
-  type: 'dismiss',
+CardWithTextOverlayVertical.args = {
+  children: (
+    <div>
+      <p className="paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <div className="bottom-bar">
+        <WiCelsius className="icon-size" />
+        <WiBarometer className="icon-size" />
+        <WiAlien className="icon-size" />
+      </div>
+    </div>
+  ),
+  type: 'text-image',
   imageURL:
     'https://images.unsplash.com/photo-1642490830096-107e6f26e172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   title: 'This is title',
+  subTitle: 'This is subtitle',
   shadow: true,
 };
+
+CardWithTextOverlayHorizontal.args = {
+  children: (
+    <div>
+      <p className="paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <div className="bottom-bar">
+        <WiCelsius className="icon-size" />
+        <WiBarometer className="icon-size" />
+        <WiAlien className="icon-size" />
+      </div>
+    </div>
+  ),
+  type: 'text-image',
+  imageURL:
+    'https://images.unsplash.com/photo-1642490830096-107e6f26e172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  title: 'This is title',
+  subTitle: 'This is subtitle',
+  shadow: true,
+  align: 'horizontal',
+};
+
+CardWithTextOverlayAndDismissHorizontal.args = {
+  children: (
+    <div>
+      <p className="paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <div className="bottom-bar">
+        <WiCelsius className="icon-size" />
+        <WiBarometer className="icon-size" />
+        <WiAlien className="icon-size" />
+      </div>
+    </div>
+  ),
+  type: 'text-image',
+  imageURL:
+    'https://images.unsplash.com/photo-1642490830096-107e6f26e172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  title: 'This is title',
+  subTitle: 'This is subtitle',
+  shadow: true,
+  align: 'horizontal',
+  dismiss: true,
+};
+
+CardWithTextOverlayAndDismissVertical.args = {
+  children: (
+    <div>
+      <p className="paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <div className="bottom-bar">
+        <WiCelsius className="icon-size" />
+        <WiBarometer className="icon-size" />
+        <WiAlien className="icon-size" />
+      </div>
+    </div>
+  ),
+  type: 'text-image',
+  imageURL:
+    'https://images.unsplash.com/photo-1642490830096-107e6f26e172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  title: 'This is title',
+  subTitle: 'This is subtitle',
+  shadow: true,
+  align: 'vertical',
+  dismiss: true,
+};
+
+CardWithOnlyText.args = {
+  children: (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <div className="bottom-bar">
+        <WiCelsius className="icon-size" />
+        <WiBarometer className="icon-size" />
+        <WiAlien className="icon-size" />
+      </div>
+    </div>
+  ),
+  title: 'This is title',
+  subTitle: 'This is subtitle',
+  shadow: true,
+};
+
+CardWithOnlyTextAndDismiss.args = {
+    children: (
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.
+        </p>
+        <div className="bottom-bar">
+          <WiCelsius className="icon-size" />
+          <WiBarometer className="icon-size" />
+          <WiAlien className="icon-size" />
+        </div>
+      </div>
+    ),
+    title: 'This is title',
+    subTitle: 'This is subtitle',
+    shadow: true,
+    dismiss: true,
+  };
+  
