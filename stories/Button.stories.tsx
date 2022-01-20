03c7eac4c6d23f86050b29080d3497b43ff89e44
launchpad5682 <launchpad5682@gmail.com>
2022-01-20 23:06:20 +0530

    feat: Button story updated

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,16 +1,47 @@
-import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Button, Props } from '../src/Button';
-
+import { Button } from '../src/Button/Button';
+import { BiAddToQueue, BiAlarm } from 'react-icons/bi';
 // metadata
 const meta: Meta = {
   title: 'Button',
   component: Button,
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
 };
 
 export default meta;
 
 // variations of the story
-export const Default = () => <Button variant="primary">Click Me</Button>;
+const Default = (args) => <Button {...args}></Button>;
 
-export const Secondary = () => <Button variant='secondary'>Click Me</Button>
+export const Solid = Default.bind({});
+export const Outline = Default.bind({});
+export const Icon = Default.bind({});
+export const FloatingAction = Default.bind({});
+
+Default.args = {
+  color: 'green',
+};
+
+Solid.args = {
+  variant: 'solid',
+  children: 'Click Me',
+};
+
+Outline.args = {
+  variant: 'outline',
+  children: 'Click Me',
+};
+
+Icon.args = {
+  variant: 'icon',
+  icon: <BiAlarm />,
+  children: 'Alarm',
+};
+
+FloatingAction.args = {
+  variant: 'floating',
+  icon: <BiAddToQueue />,
+  children: null,
+};
