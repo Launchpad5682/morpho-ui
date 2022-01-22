67e646dbbfabcf4317dd132f7e6a5247ec6f796e
launchpad5682 <launchpad5682@gmail.com>
2022-01-22 19:06:19 +0530

    docs: Snackbar documentation added

--- src/SnackBar/SnackBar.tsx
+++ src/SnackBar/SnackBar.tsx
@@ -11,6 +11,24 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
   onClose?: MouseEventHandler;
 }
 
+/**
+ * SnackBar must be used in the main component as it needs
+ * to be absolute to viewports with global state to on/off
+ * when needed.
+ * @param variant
+ * Choose a variant from baseline, stacked and leading.
+ * @param message
+ * Enter a message for snackbar.
+ * @param buttonText
+ * Enter text for button
+ * @param onButtonPress
+ * Add handler for button to perform someting.
+ * @param onClose
+ * Add handler for cross/close icon. It can contain the
+ * state for switching on/off
+ * @returns Snackbar
+ */
+
 export const SnackBar = ({
   variant,
   message,
