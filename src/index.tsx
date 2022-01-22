2ae3b971e9b9f29a404bbd4b1feb2412eb1bbbae
launchpad5682 <launchpad5682@gmail.com>
2022-01-22 12:38:10 +0530

    feat: Chips component added to index

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,5 @@ export { Avatar } from './Avatar/Avatar';
 export { Badge } from './Badge/Badge';
 export { Button } from './Button/Button';
 export { Card } from './Card/Card';
+export { Chips } from './Chips/Chips';
 export { Image } from './Image/Image';
