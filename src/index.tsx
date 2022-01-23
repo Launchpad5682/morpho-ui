5f57885d73a727f7db0f1b5fa96406dc4d462c6d
launchpad5682 <launchpad5682@gmail.com>
2022-01-23 14:53:29 +0530

    refactor: all the components moved to components directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
-export { Alert } from './Alert/Alert';
-export { Avatar } from './Avatar/Avatar';
-export { Badge } from './Badge/Badge';
-export { Button } from './Button/Button';
-export { Card } from './Card/Card';
-export { Chips } from './Chips/Chips';
-export { Image } from './Image/Image';
-export { Modal } from './Modal/Modal';
-export { SnackBar } from './SnackBar/SnackBar';
-export { Typography } from './Typography/Typography';
+export { Alert } from './components/Alert/Alert';
+export { Avatar } from './components/Avatar/Avatar';
+export { Badge } from './components/Badge/Badge';
+export { Button } from './components/Button/Button';
+export { Card } from './components/Card/Card';
+export { Chips } from './components/Chips/Chips';
+export { Image } from './components/Image/Image';
+export { Modal } from './components/Modal/Modal';
+export { SnackBar } from './components/SnackBar/SnackBar';
+export { Typography } from './components/Typography/Typography';
+
+/**Custom NavLink for different style on activeClassname */
+export { CustomNavLink } from './helper/CustomNavLink/CustomNavLink';
