85d1921dec37dc134302486df85ff3b4f255b787
launchpad5682 <launchpad5682@gmail.com>
2022-01-23 18:43:33 +0530

    fix: build fix, created component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,7 +1,9 @@
 import { HTMLAttributes, ReactNode } from 'react';
-
+import React from 'react';
 export interface Props extends HTMLAttributes<HTMLInputElement> {
-    children: ReactNode;
-    
+  children: ReactNode;
 }
 
+export const Input = () => {
+  return <div></div>;
+};
---
+++ src/components/Input/TextBox.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const TextBox = () => {
+  return <div></div>;
+};
