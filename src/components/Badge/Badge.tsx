7621fb4ade2a883b112c767f7244e0cdb0c88084
launchpad5682 <launchpad5682@gmail.com>
2022-01-28 17:50:22 +0530

    refactor: new sizes added to Badge and click event added

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,24 +1,49 @@
 import './Badge.css';
-import React, { HTMLAttributes, ReactNode } from 'react';
+import React, { HTMLAttributes, MouseEventHandler, ReactNode } from 'react';
 
 export interface Props extends HTMLAttributes<HTMLSpanElement> {
   children: ReactNode;
   variant: 'avatar' | 'icon';
+  color: 'red' | 'green' | 'yellow' | 'blue' | 'pink';
   number: Number;
   mode?: 'online' | 'offline';
+  onClickBadge?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export const Badge = ({ children, variant, number, mode }: Props) => {
+/**
+ *
+ * @param variant
+ * avatar: use sm-avatar or md-avatar for avatar variant badge
+ * <Avatar size="sm-avatar" />
+ * @returns
+ */
+export const Badge = ({
+  children,
+  variant,
+  color = 'blue',
+  number,
+  mode,
+  onClickBadge = () => {
+    console.log('badge clicked');
+  },
+}: Props) => {
   return (
     <button
-      className={`${variant === 'avatar' ? 'badge__avatar' : 'badge__icon'}`}
+      onClick={onClickBadge}
+      className={`${
+        variant === 'avatar'
+          ? 'badge__avatar'
+          : `badge__icon badge__icon--${color}`
+      }`}
     >
       {variant === 'icon' ? (
-        <span className="badge__icon--counter">{number}</span>
+        <span className={`badge__icon--counter icon__counter--${color}`}>
+          {number}
+        </span>
       ) : mode === 'online' ? (
         <span className="badge_avatar badge__avatar--online" />
       ) : (
-        <span className="badge_avatar badge__avatar--offline"/>
+        <span className="badge_avatar badge__avatar--offline" />
       )}
       {children}
     </button>
