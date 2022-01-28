5aab5a66113e0ed7e8cb18e4cb2c8201ecb399a5
launchpad5682 <launchpad5682@gmail.com>
2022-01-28 16:01:25 +0530

    style: added xl sized avatar and resized every available siz3

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -4,13 +4,13 @@ import './Avatar.css';
 
 export interface Props extends HTMLAttributes<HTMLSpanElement> {
   imageURL?: string;
-  size?: 'sm-avatar' | 'md-avatar' | 'lg-avatar';
+  size?: 'sm-avatar' | 'md-avatar' | 'lg-avatar' | 'xl-avatar';
   type?: 'round' | 'square';
 }
 
 /**
- * @param imageURL 
- * enter the image url you want to use in the avatar, 
+ * @param imageURL
+ * enter the image url you want to use in the avatar,
  * @param size
  * choose from any of three sm-avatar, md-avatar, and lg-avatar,
  * @param type
@@ -18,7 +18,7 @@ export interface Props extends HTMLAttributes<HTMLSpanElement> {
  * @returns Avatar component
  */
 export const Avatar = ({
-  imageURL = "https://images.unsplash.com/photo-1602870049368-9c8265ff3693?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+  imageURL = 'https://images.unsplash.com/photo-1602870049368-9c8265ff3693?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
   size = 'sm-avatar',
   type = 'round',
 }: Props) => (
