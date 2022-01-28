18892b89020b5b4748f4c35f6039d5b285358d7a
launchpad5682 <launchpad5682@gmail.com>
2022-01-29 00:45:42 +0530

    refactor: Alert updated with 5 color options and typography

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,12 +1,23 @@
-import React, { HTMLAttributes, ReactNode } from 'react';
+import React, { HTMLAttributes, MouseEventHandler, ReactNode } from 'react';
+import { VscClose } from 'react-icons/vsc';
+import { Typography } from '../..';
 import './Alert.css';
 
 export interface Props extends HTMLAttributes<HTMLParagraphElement> {
   children: ReactNode;
-  color?: 'red' | 'green' | 'yellow' | 'blue';
-  size?: 'sm' | 'md' | 'lg';
+  color?: 'red' | 'green' | 'yellow' | 'blue' | 'pink';
+  onClose?: MouseEventHandler;
 }
 
-export const Alert = ({ children, color = 'red', size = 'sm' }: Props) => (
-  <p className={`${color} ${size} alert-box`}>{children}</p>
+export const Alert = ({ children, color = 'red', onClose }: Props) => (
+  <div className={`alert--${color} alert--box`}>
+    <Typography variant="subtitle2" textColor={color}>
+      {children}
+    </Typography>
+    <Typography variant="subtitle2" textColor={color}>
+      <VscClose onClick={onClose} />
+    </Typography>
+  </div>
 );
+
+/**className={`${color} ${size} alert-box`} */
